d552a70d878b8886f205c7a2d0bfbcb84b6c245f
rosenhaim <rosenhaim@gmail.com>
2022-03-01 19:40:51 -0300

    confirm

---
+++ src/BlaBla.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+
+
+class BlaBla extends Component {
+   render(){
+    return (
+        <div>
+           
+               Nome : {this.props.name}
+           
+          
+
+             
+        </div>
+       
+        );   
+  
+
+   }
+     
+      
+
+}
+
+
+export default BlaBla
---
+++ src/Rosen.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import BlaBla from './BlaBla';
+
+
+
+class Rosen extends Component {
+   render(){
+    return (
+        <div>
+           
+                euuuuu aaa
+           
+          <BlaBla></BlaBla>
+
+             
+        </div>
+       
+        );   
+  
+
+   }
+     
+      
+
+}
+
+
+export default Rosen;
